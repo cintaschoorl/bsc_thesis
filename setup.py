0c786018ac2582c2ba93ce8712e984fc4759d2aa
cintaschoorl <schoorlcinta@gmail.com>
2025-04-20 20:25:25 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+# setting up new conda environment
+$ conda create -n thesisenv python=3.10 -y
+$ conda activate thesisenv
+
+# install necessary packages and tools
+$ pip install whitebox
+$ pip install numpy pandas matplotlib

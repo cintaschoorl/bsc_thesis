d848425e76f28aa382a1864723872246d674f84f
cintaschoorl <schoorlcinta@gmail.com>
2025-04-22 13:22:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 # setting up new conda environment
-$ conda create -n thesisenv python=3.10 -y
-$ conda activate thesisenv
+conda create -n thesisenv python=3.10 -y
+conda activate thesisenv
 
 # install necessary packages and tools
-$ pip install notebook
-$ pip install whitebox
-$ pip install numpy pandas matplotlib
+pip install notebook
+pip install whitebox
+pip install numpy pandas matplotlib
 
 # navigate to repo-path and activate:
-$ cd ~/OneDrive/Documents/School/FPS/Scriptie/bsc_thesis
-$ conda activate thesisenv
+cd ~/OneDrive/Documents/School/FPS/Scriptie/bsc_thesis
+conda activate thesisenv
+
+
+# arcgis conda environment for arcpy etc
+conda create -n arcgisenv python=3.11 -y
+conda activate arcgisenv
 
